bcbe50f06f3fc6b3be6a816e74d753838a00662a
Artem Kaloev <kaloev.eltech@gmail.com>
2017-12-05 13:07:39 +0300

    2.changed-cmake

--- cmake-build-debug/CMakeFiles/Course3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Course3.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/mi/CLionProjects/Course3/main.cpp" "C:/Users/mi/CLionProjects/Course3/cmake-build-debug/CMakeFiles/Course3.dir/main.cpp.obj"
+  "/Users/artemkaloev/Desktop/study/Course3/main.cpp" "/Users/artemkaloev/Desktop/study/Course3/cmake-build-debug/CMakeFiles/Course3.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- cmake-build-debug/CMakeFiles/Course3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Course3.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Course3.dir/main.cpp.obj"
+  "CMakeFiles/Course3.dir/main.cpp.o"
   "Course3.pdb"
-  "Course3.exe"
-  "Course3.exe.manifest"
-  "libCourse3.dll.a"
+  "Course3"
 )
 
 # Per-language clean rules from dependency scanning.
